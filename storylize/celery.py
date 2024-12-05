3f0799104550b87217d7a4ac9a0432067b3b5f2b
Kabaki Antony <kabaki.antony@gmail.com>
2024-12-05 12:21:46 +0300

    create celery app and initialize it

--- storylize/celery.py
+++ storylize/celery.py
@@ -1,13 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storylize.settings')
 
-app = Celery('proj')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+celery_app = Celery('storylize')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
